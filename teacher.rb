24040ddf6bf35504f7b7abd3f65cf095ac0b2fd8
Adeshina Adenuga <Nuga0909@gmail.com>
2023-11-07 09:19:41 +0100

    fix linter errors

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,10 @@
-require "./person.rb"
+require './person'
 
 class Teacher < Person
-    def initialize(age, specialization, name = 'unknown', parent_permission: true)
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
+  def initialize(age, specialization, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
   def can_use_services?
     true
