0aca786961b09a3c91324cf9d9743aa885fb2883
Adeshina Adenuga <Nuga0909@gmail.com>
2023-11-08 19:45:27 +0100

    fix linter errors

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-        raise NotImplementedError, 'implement this method'
-    end
+  def correct_name
+    raise NotImplementedError, 'implement this method'
+  end
 end
