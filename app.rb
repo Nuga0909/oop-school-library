a0ae3640608f1dec5c4608c7cbb584e148c90a71
Adeshina Adenuga <Nuga0909@gmail.com>
2023-11-10 14:18:36 +0100

    add methos to creaate person, teacher and student

--- app.rb
+++ app.rb
@@ -34,3 +34,43 @@ class App
       end
     end
   end
+
+
+  def person_create
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    number = gets.chomp
+    case number
+    when '1'
+      student_create
+    when '2'
+      teacher_create
+    else
+      puts 'Invalid entry'
+    end
+  end
+
+  def teacher_create
+    puts 'Age:'
+    age = gets.chomp
+    puts 'Name:'
+    name = gets.chomp
+    puts 'Specialization:'
+    specialization = gets.chomp
+    parent_permission = true
+    @people.push Teacher.new(age: age, name: name, specialization: specialization, parent_permission: parent_permission)
+    puts 'Person created successfully!'
+  end
+
+  def student_create
+    puts 'Age:'
+    age = gets.chomp
+    puts 'Name:'
+    name = gets.chomp
+    puts 'Has parent permission? [y/n]'
+    permission = gets.chomp.downcase
+    parent_permission = permission == 'y'
+    puts 'Classroom'
+    classroom = gets.chomp
+    @people.push Student.new(age: age, name: name, parent_permission: parent_permission, classroom: classroom)
+    puts 'Person created successfully!'
+  end
